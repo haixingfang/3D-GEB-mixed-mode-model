d5db58d661071bc12484492650a05c4ca9506437
haixingfang <haixingfang868@gmail.com>
2021-01-20 22:10:45 +0100

    update

--- ferrite_3d_model_voronoin_PBC_ND_CNT_GEB.m
+++ ferrite_3d_model_voronoin_PBC_ND_CNT_GEB.m
@@ -458,7 +458,7 @@ elseif CyclicFlag==0
 else
     dt(1)=tcr(end)/100; % for continuous cooling, about 3.5 h
 end
-dx=[2 0.005]; % max and min step size in length [um]
+dx=[1 0.005]; % max and min step size in length [um]
 DiffInfo=cell([1 length(N_p(:,1))]);
 
 while stop_cycle~=1 % main loop to calculate nucleation,growth and call impingement function
@@ -1233,6 +1233,11 @@ while stop_cycle~=1 % main loop to calculate nucleation,growth and call impingem
 %%%%%%%%%%%%%%%%%%%%%%%%%
     if T(i)<=Temp_eq(5,2)||Timer(i)>tcr(end)
         stop_cycle=1;
+    end
+    if CyclicFlag==0 && i>50
+        if abs(F(i)-F(i-1))<=1e-5 && (N_eff(i)-N_eff(i-1))==0 && (N_eff(i)-N_eff(i-50))==0
+            stop_cycle=1;
+        end
     end
     t=t+dt(i);
 end
